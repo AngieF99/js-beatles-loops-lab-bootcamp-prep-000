33ca9a55d8140512114a4069e997af09a0ee2d84
Angelica Fields <angelicafields08@gmail.com>
2018-05-31 16:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ function theBeatlesPlay(musicians,instruments) {
   return array
 }
 
-function johnLennonFacts(array) {
-       const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+function johnLennonFacts(facts) {
+ var array = [];
   while ( array < facts.length) {
     array.push(`${facts}!!!`)
   }
